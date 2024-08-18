27de42b53d94cf0c3c0b74e4563a4d182129b427
debangan13 <debangan.barmanslg@gmail.com>
2024-08-18 19:39:11 +0530

    fixed some bug

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { toast } from "sonner"
 import { useAppStore } from "@/store";
+import ContactsContainer from "./components/contacts-container";
+import EmptyChatConatiner from "./components/empty-chat-container";
+import ChatContainer from "./components/chat-container";
 
 const Chat = () => {
  const {userInfo} = useAppStore()
@@ -13,8 +16,10 @@ const Chat = () => {
     }
   },[userInfo,navigate])
   return (    
-    <div>
-      Chat
+    <div className="flex h-[100vh] text-white overflow-hidden ">
+      <ContactsContainer/>
+      {/* <EmptyChatConatiner/> */}
+      <ChatContainer/>
     </div>
   )
 }
