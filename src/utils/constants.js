72672b6324cbf7faae924d1241a4cfcdfb942f3e
debangan13 <debangan.barmanslg@gmail.com>
2024-08-19 05:28:34 +0530

    added logut route

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,3 +8,4 @@ export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`
 export const UPLOAD_PROFILE_IMAGE = `${AUTH_ROUTES}/upload-profile-image`
 export const REMOVE_PROFILE_IMAGE = `${AUTH_ROUTES}/remove-profile-image`
 export const GET_PROFILE_IMAGE = `${AUTH_ROUTES}/get-profile-image`
+export const LOGOUT_ROUTE = `${AUTH_ROUTES}/logout`
